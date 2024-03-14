25127b91e9b100a686bf965caba72247f0bcd346
bart3br <95648151+bart3br@users.noreply.github.com>
2024-03-14 22:33:31 +0100

    small changes

--- Lab1/classes.py
+++ Lab1/classes.py
@@ -1,4 +1,5 @@
 import math
+import datetime
 
 # instance of a Stop class represents one Stop with defined name, lattitude and longtitude
 class Stop:
@@ -10,7 +11,7 @@ class Stop:
     def __str__(self):
         return f"{self.name} at {self.lat}, {self.lon}"
     
-    def EuclideanDist(self, other: Stop):
+    def EuclideanDist(self, other: 'Stop'):
         return math.sqrt(math.pow(self.lat - other.lat, 2) + math.pow(self.lon - other.lon, 2))
 
 
@@ -23,4 +24,10 @@ class Route:
         self.arrivalTime = kwargs["arrival_time"]
         self.startStop = Stop(kwargs["start_stop"], kwargs["start_stop_lat"], kwargs["start_stop_lon"])
         self.endStop = Stop(kwargs["end_stop"], kwargs["end_stop_lat"], kwargs["end_stop_lon"])
+        
+    def __str__(self):
+        return f"Line {self.line}, from {self.startStop.name} to {self.endStop.name}"
 
+
+if __name__ == "__main__":
+    pass
--- Lab1/preprocessing.py
+++ Lab1/preprocessing.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import csv
 import datetime
+from classes import Route
 
+filename = "connection_graph.csv"
 col_types = {
     'id':int,
     'company': str,
@@ -14,26 +15,54 @@ col_types = {
     'start_stop_lon': float,
     'end_stop_lat': float,
     'end_stop_lon': float
-    }
+}
 
-data = pd.read_csv("connection_graph.csv", dtype=col_types)
-#data1 = csv.reader('connection_graph.csv')
-data.rename(columns={data.columns[0]: "id"}, inplace=True)
-#print(data.columns)
-#print(data.head(10))
+# data from connection_graph.csv file refers to 1st of March 2023
+date = "01.03.2023"
+date_format = "%d.%m.%Y"
+time_format = "%H:%M:%S"
 
-for type in data.dtypes:
-    print(type)
+
+def read_data():
+    data = pd.read_csv("connection_graph.csv", dtype=col_types)
+    #add missing id column name
+    data.rename(columns={data.columns[0]: "id"}, inplace=True)
+    return data
+
+
+def data_row_to_route_tuple(row):
+    route = Route(
+        company=row["company"], 
+        line=row["line"], 
+        departure_time=row["departure_time"],
+        arrival_time=row["arrival_time"],
+        start_stop=row["start_stop"],
+        start_stop_lat=row["start_stop_lat"],
+        start_stop_lon=row["start_stop_lon"],
+        end_stop=row["end_stop"],
+        end_stop_lat=row["end_stop_lat"],
+        end_stop_lon=row["end_stop_lon"]
+    )
+    return (row["id"], route)
+
+def preprocess(data: pd.DataFrame) -> dict:
+    dictionary = {}
     
-# data = data.astype(col_types, errors='ignore')
-# for type in data.dtypes:
-#     print(type)
+    for index, row in data.iterrows():
+        tup = data_row_to_route_tuple(row)
+        dictionary[tup[0]] = tup[1]
     
-print(data.head(10))
+    return dictionary
+
+def temp():
+    data = read_data()
+    dictionary = preprocess(data)
+    for key, value in dictionary.items():
+        print (key, value)
+    
 
 
-
-
-#df = pd.DataFrame(data)
-#df.head(10)
-#df.dtypes
+if __name__ == "__main__":
+    data = pd.read_csv('connection_graph.csv', dtype=col_types)
+    print(data.head(10))
+    temp()
