f4eeb9521a6c2a7883a4eb6f2cbf17751f95c25a
bart3br <95648151+bart3br@users.noreply.github.com>
2024-05-22 19:24:51 +0200

    initial files and venv configuration

---
+++ Lab3/facts_and_rules.py
@@ -0,0 +1,13 @@
+from experta import Fact, KnowledgeEngine
+
+class MicrowaveFact(Fact):
+    """provides information about microwave state,
+       used by expert system to check the rules conditions"""
+    pass
+
+class MicrowaveExpertSystem(KnowledgeEngine):
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ Lab3/main.py
@@ -0,0 +1,7 @@
+from experta import *
+
+def diagnose_microwave_issues():
+    pass
+
+if __name__ == "__main__":
+    pass
